31cafe18315953da4210606356373f2d5784192c
Sushma Varadaiah <sushmarajuvaradaiah@gmail.com>
2020-03-31 20:40:37 +0530

    Register company smart contract

--- network/scripts/updateChaincode.sh
+++ network/scripts/updateChaincode.sh
@@ -7,21 +7,21 @@ echo "\___ \    | |     / _ \   | |_) |   | |  "
 echo " ___) |   | |    / ___ \  |  _ <    | |  "
 echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
 echo
-echo "Updating Chaincode CERTNET On Certification Network"
+echo "Updating Chaincode Pharmanet On Pharma Network"
 echo
 CHANNEL_NAME="$1"
 DELAY="$2"
 LANGUAGE="$3"
 VERSION="$4"
 TYPE="$5"
-: ${CHANNEL_NAME:="certificationchannel"}
+: ${CHANNEL_NAME:="pharmachannel"}
 : ${DELAY:="5"}
 : ${LANGUAGE:="node"}
 : ${VERSION:=1.1}
 : ${TYPE="basic"}
 
 LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
-ORGS="iit mhrd upgrad"
+ORGS="consumer distributor manufacturer retailer transporter"
 TIMEOUT=15
 
 if [ "$TYPE" = "basic" ]; then
@@ -36,19 +36,23 @@ echo "New Version : "$VERSION
 . scripts/utils.sh
 
 ## Install new version of chaincode on peer0 of all 3 orgs making them endorsers
-echo "Updating chaincode on peer0.iit.certification-network.com ..."
-installChaincode 0 'iit' $VERSION
-echo "Updating chaincode on peer0.mhrd.certification-network.com ..."
-installChaincode 0 'mhrd' $VERSION
-echo "Updating chaincode on peer0.upgrad.certification-network.com ..."
-installChaincode 0 'upgrad' $VERSION
+echo "Updating chaincode on peer0.manufacturer.pharma-supply-network.com ..."
+installChaincode 0 'manufacturer' $VERSION
+echo "Updating chaincode on peer0.distributor.pharma-supply-network.com ..."
+installChaincode 0 'distributor' $VERSION
+echo "Updating chaincode on peer0.retailer.pharma-supply-network.com ..."
+installChaincode 0 'retailer' $VERSION
+echo "Updating chaincode on peer0.transporter.pharma-supply-network.com ..."
+installChaincode 0 'transporter' $VERSION
+echo "Updating chaincode on peer0.consumer.pharma-supply-network.com ..."
+installChaincode 0 'consumer' $VERSION
 
 # Upgrade chaincode on the channel using peer0.iit
-echo "Upgrading chaincode on channel using peer0.iit.certification-network.com ..."
-upgradeChaincode 0 'iit' $VERSION
+echo "Upgrading chaincode on channel using peer0.manufacturer.pharma-supply-network.com ..."
+upgradeChaincode 0 'manufacturer' $VERSION
 
 echo
-echo "========= All GOOD, Chaincode CERTNET Is Now Updated To Version '$VERSION' =========== "
+echo "========= All GOOD, Chaincode Pharanet Is Now Updated To Version '$VERSION' =========== "
 echo
 
 echo

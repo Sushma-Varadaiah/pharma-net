1456965ae4d965fdc87fb974232643e8bf6ae4da
Sushma Varadaiah <sushmarajuvaradaiah@gmail.com>
2020-04-01 16:09:50 +0530

    add drug,view drug,view drug history functions

--- chaincode/contract.js
+++ chaincode/contract.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const { Contract } = require("fabric-contract-api");
+const ClientIdentity = require("fabric-shim").ClientIdentity;
+const util = require("util");
 
 class PharmanetContract extends Contract {
   constructor() {
@@ -16,51 +18,166 @@ class PharmanetContract extends Contract {
     console.log("Pharmanet Smart Contract Instantiated Super successful");
   }
 
-  async getMe(ctx) {
-    return "hello sushma";
-  }
-
-  async getMeAtLast(ctx, name) {
-    let a = "hello" + name + "blablabla success";
-    return a;
-  }
-
   async registerCompany(ctx, companyCRN, companyName, location, organisationRole) {
     console.log("Registering the company " + companyName);
 
-    const companyID = ctx.stub.createCompositeKey("org.pharma-network.pharmanet.company", [companyCRN, companyName]);
+    let cid = new ClientIdentity(ctx.stub);
+    let mspID = cid.getMSPID();
 
-    if (
-      organisationRole !== "Manufacturer" &&
-      organisationRole !== "Distributor" &&
-      organisationRole !== "Retailer" &&
-      organisationRole !== "Transporter"
-    ) {
-      return "Invalid Organisation Role";
-    } else {
-      let hierarchyKey;
-      if (organisationRole === "Manufacturer") {
-        hierarchyKey = "1";
-      } else if (organisationRole === "Distributor") {
-        hierarchyKey = "2";
-      } else if (organisationRole === "Retailer") {
-        hierarchyKey = "3";
-      } else if (organisationRole === "Transporter") {
-        hierarchyKey = "";
+    console.log("MSPID of the transaction initiator is=> " + mspID);
+
+    if ("consumerMSP" !== mspID) {
+      const companyID = ctx.stub.createCompositeKey("org.pharma-network.pharmanet.company", [companyCRN, companyName]);
+
+      if (
+        organisationRole !== "Manufacturer" &&
+        organisationRole !== "Distributor" &&
+        organisationRole !== "Retailer" &&
+        organisationRole !== "Transporter"
+      ) {
+        return "Invalid Organisation Role";
+      } else {
+        let hierarchyKey;
+        if (organisationRole === "Manufacturer") {
+          hierarchyKey = "1";
+        } else if (organisationRole === "Distributor") {
+          hierarchyKey = "2";
+        } else if (organisationRole === "Retailer") {
+          hierarchyKey = "3";
+        } else if (organisationRole === "Transporter") {
+          hierarchyKey = "";
+        }
+        let companyObject = {
+          companyID: companyID,
+          name: companyName,
+          location: location,
+          organisationRole: organisationRole,
+          hierarchyKey: hierarchyKey,
+          createdAt: new Date()
+        };
+
+        let companyDataBuffer = Buffer.from(JSON.stringify(companyObject));
+        await ctx.stub.putState(companyID, companyDataBuffer);
+        return companyObject;
       }
-      let companyObject = {
-        companyID: companyID,
-        name: companyName,
-        location: location,
-        organisationRole: organisationRole,
-        hierarchyKey: hierarchyKey,
-        createdAt: new Date()
-      };
-
-      let companyDataBuffer = Buffer.from(JSON.stringify(companyObject));
-      await ctx.stub.putState(companyID, companyDataBuffer);
-      return companyObject;
     }
+  }
+
+  async addDrug(ctx, drugName, serialNo, mfgDate, expDate, companyCRN) {
+    console.info(" Add Drug ", drugName, serialNo, mfgDate, expDate, companyCRN);
+
+    let companyResultsIterator = await ctx.stub.getStateByPartialCompositeKey("org.pharma-network.pharmanet.company", [
+      companyCRN
+    ]);
+
+    // Iterate through result set and for each company found.
+    while (true) {
+      let responseRange = await companyResultsIterator.next();
+      console.log("responseRange=> " + responseRange);
+      if (!responseRange || !responseRange.value || !responseRange.value.key) {
+        return "Invalid companyCRN";
+      }
+      console.log("ResponseRange.value.key=>" + responseRange.value.key);
+
+      let objectType;
+      let attributes;
+      ({ objectType, attributes } = await ctx.stub.splitCompositeKey(responseRange.value.key));
+
+      let returnedCompanyName = attributes[0];
+      let returnedCompanyCRN = attributes[1];
+
+      // index:org.pharma-network.pharmanet.company namespace:CRN1 companyname:Digi
+      console.info(
+        util.format(
+          "- found a company from namespace:%s companyname:%s companycrn:%s\n",
+          objectType,
+          returnedCompanyName,
+          returnedCompanyCRN
+        )
+      );
+
+      let cid = new ClientIdentity(ctx.stub);
+      let mspID = cid.getMSPID();
+
+      console.log("MSPID of the transaction initiator is=> " + mspID);
+
+      //let isManufacturer = false;
+
+      if ("manufacturerMSP" === mspID) {
+        //isManufacturer = true;
+        console.log("yes he is a manufacturer");
+
+        const productID = ctx.stub.createCompositeKey("org.pharma-network.pharmanet.drug", [drugName, serialNo]);
+
+        // const manufacturerId = ctx.clientIdentity.getID();
+        // console.log("manufacturerId is=> " + manufacturerId.toString());
+        // const manufactureID = ctx.stub.createCompositeKey([manufacturerId]);
+
+        //create the drug object to store on the ledger
+        let drugObject = {
+          productID: productID,
+          name: drugName,
+          manufacturer: ctx.clientIdentity.getID(),
+          manufacturingDate: mfgDate,
+          expiryDate: expDate,
+          owner: ctx.clientIdentity.getID(),
+          shipment: ""
+        };
+
+        console.log("drugObject created is==> " + drugObject);
+
+        let drugDataBuffer = Buffer.from(JSON.stringify(drugObject));
+        await ctx.stub.putState(productID, drugDataBuffer);
+        return drugObject;
+      } else {
+        //isManufacturer = false;
+        return "No one can add a drug but Manufacturer.";
+        console.log("No. Not a a Manufacturer");
+      }
+    }
+  }
+  //get details of the transaction
+  // async getDetailsOfTheTransactionInitiator(ctx) {
+  //   let cid = new ClientIdentity(ctx.stub);
+
+  //   let mspID = cid.getMSPID();
+  //   console.log("MSPID of the transaction initiator is=> " + mspID);
+
+  //   let isManufacturer = false;
+
+  //   if ("manufacturerMSP" === mspID) {
+  //     isManufacturer = true;
+  //     console.log("yes he is a manufacturer");
+  //   } else {
+  //     isManufacturer = false;
+  //     console.log("No. Not a a Manufacturer");
+  //   }
+  // }
+
+  async viewDrugCurrentState(ctx, drugName, serialNo) {
+    //Visible for all network participants
+    const productID = ctx.stub.createCompositeKey("org.pharma-network.pharmanet.drug", [drugName, serialNo]);
+    console.log("ProductID is=> " + productID);
+    let drugDataBuffer = await ctx.stub.getState(productID).catch(err => console.log(err));
+    return JSON.parse(drugDataBuffer.toString());
+  }
+
+  async viewHistory(ctx, drugName, serialNo) {
+    const productID = ctx.stub.createCompositeKey("org.pharma-network.pharmanet.drug", [drugName, serialNo]);
+    console.info("getting history for key: " + productID);
+    let iterator = await ctx.stub.getHistoryForKey(productID);
+    let result = [];
+    let res = await iterator.next();
+    while (!res.done) {
+      if (res.value) {
+        console.info(`found state update with value: ${res.value.value.toString("utf8")}`);
+        const obj = JSON.parse(res.value.value.toString("utf8"));
+        result.push(obj);
+      }
+      res = await iterator.next();
+    }
+    await iterator.close();
+    return result;
   }
 }
 
